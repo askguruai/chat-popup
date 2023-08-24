e4546e989e64d3b420e034db6d90ca1f2797d6bc
SergeiKanerva <49556048+SergeiKanerva@users.noreply.github.com>
2023-08-25 00:09:37 +0400

    Messages width, color, marked config, logo

--- chat-application/src/components/Message.js
+++ chat-application/src/components/Message.js
@@ -3,6 +3,23 @@ import { useEffect, useRef } from 'react';
 export default function Message({ data }) {
   const markdownRef = useRef(null);
 
+  useEffect(() => {
+    const tokenizer = new marked.Tokenizer();
+    const renderer = new marked.Renderer();
+    tokenizer.lheading = function () {
+      return false;
+    };
+    renderer.link = function (href, title, text) {
+      return `<a target="_blank" href="${href}">${text}` + '</a>';
+    };
+    marked.setOptions({
+      tokenizer: tokenizer,
+      renderer: renderer,
+      headerIds: false,
+      mangle: false,
+    });
+  }, []);
+
   const compileMarkdown = (messageMarkdown) => {
     return marked(messageMarkdown);
   };
