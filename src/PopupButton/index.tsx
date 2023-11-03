e4d6d563e5c1af2a974858ee2adb11aee458c100
Alexander Smirnov <furiousteabag@gmail.com>
2023-11-03 18:16:55 +0530

    cleanup

--- src/PopupButton/index.tsx
+++ src/PopupButton/index.tsx
@@ -25,7 +25,7 @@ export default function PopupButton({
   function handleClick(): void {
     setIsCollapsed(!isCollapsed)
     setHasInteracted(true)
-    // localStorage.setItem("askguru-has-interacted", "true")
+    localStorage.setItem("askguru-has-interacted", "true")
     askguruAPI.logEvent({ eventType: "POPUP_CALLED" })
   }
 
